063ab00f5c608f1914c9d275e749d149ef6a625e
Andriyan Harizanov <81181412+AndrHary@users.noreply.github.com>
2022-01-11 16:06:26 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './style.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [days, setDays] = useState(0)
+    return (
+        <section id="home">
+            <div class="title-box">
+                <h1>Tandems</h1>
+                <h2>Find the pair of employees who have worked as tandem for the longest amount of time.</h2>
+            </div>
+            <div class="file-selection">
+                <h3>Choose file from here </h3>
+                <div class="input-container">
+                    <label for="files"><i class="fas fa-folder-plus"></i></label>
+                    <input type="file" id="fileInput" name="files" />
+                </div>
+            </div>
+            <div id="wrapper"></div>
+        </section>
+    )
 }
 
-export default App;
+export default App
